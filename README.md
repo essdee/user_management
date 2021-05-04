bd779031d1c37eac9e426adb82c8ced99f04df66
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-04 15:17:42 +0530

    Initialize app

---
+++ README.md
@@ -0,0 +1,7 @@
+## User Management
+
+Frappe app to handle user management
+
+#### License
+
+MIT
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in user_management/__init__.py
+from user_management import __version__ as version
+
+setup(
+	name='user_management',
+	version=version,
+	description='Frappe app to handle user management',
+	author='Essdee',
+	author_email='essdee@gmail.com',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
